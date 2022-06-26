5cb628648445d62002124f3417bd955e8c08399a
Kucin-Oyen <108226915+Kucing0yen@users.noreply.github.com>
2022-06-26 21:56:24 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,27 @@
+import os,sys,time,requests,json,random
+from colorama import Fore,init,Back
+
+B = Fore.BLUE
+W = Fore.WHITE
+R = Fore.RED
+G = Fore.GREEN
+BL = Fore.BLACK
+Y = Fore.YELLOW
+Hijau="\033[1;92m"
+putih="\033[1;97m"
+abu="\033[1;90m"
+kuning="\033[1;93m"
+ungu="\033[1;95m"
+biru="\033[1;96m"
+#Tulisan Background Merah
+bg="\033[1;0m\033[1;41mText\033[1;0m"
+
+def banner():
+	print(f"""
+{ungu}╔╦╗{W}┌─┐┌─┐┌─┐  {biru}╔═╗{W}┌┬┐┌─┐
+{ungu}║║║{W}├─┤└─┐└─┐  {biru}╚═╗{W}│││└─┐
+{ungu}╩ ╩{W}┴ ┴└─┘└─┘  {biru}╚═╝{W}┴ ┴└─┘
+""")
+
+banner()
+time.sleep(8)
